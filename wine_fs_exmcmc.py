10c86fa4d91a24b4ce9891611fb3603f641ab101
Hayaru SHOUNO <shouno@uec.ac.jp>
2017-10-08 17:42:12 +0900

    とりあえず，使えそうになったバージョン

--- wine_fs_exmcmc.py
+++ wine_fs_exmcmc.py
@@ -9,11 +9,11 @@ from tqdm import tqdm
 from sklearn.model_selection import StratifiedKFold
 from sklearn.svm import LinearSVC
 from sklearn.datasets import load_wine
-# from joblib import Parallel, delayed
-# from numba import jit
+from joblib import Parallel, delayed
+# from numba import jit, f8
 
 
-# @jit(nopython=True, cache=True)
+# クラス内においておくと激オソなので外に引っ張り出しておく
 def CVError(sval, X, y):
         '''Definition of 'Energy function', that is a CV err
         input sval, dset
@@ -31,7 +31,7 @@ def CVError(sval, X, y):
         for trn, tst in skf.split(X, y):
             clsf.fit(X[trn][:, cidx], y[trn])
             pred = clsf.predict(X[tst][:, cidx])
-            scrs.append(1. - np.sum(y[tst] == pred) / (y[tst].shape[0]))
+            scrs.append(- np.sum(y[tst] == pred) / (y[tst].shape[0]))
         scrs = np.array(scrs)
         return scrs.mean()
 
@@ -48,7 +48,7 @@ def MCstep(sval, energy, beta, X, y):
             sval = binomial(1, 0.5, size=size) * 2 - 1.
             energy = size * CVError(sval, X, y)
             acccnt = size
-            return sval, energy, acccnt
+            return energy, acccnt
         # 有限温度の場合
         order = permutation(size)
         rvals = uniform(0., 1., size)
@@ -67,7 +67,7 @@ def MCstep(sval, energy, beta, X, y):
             else:
                 # 'reject' restore
                 sval[idx] *= -1
-        return sval, energy, acccnt
+        return energy, acccnt
 
 
 class FSsingleMC:
@@ -82,59 +82,9 @@ class FSsingleMC:
         self.nsplits = nsplits
         self.s = binomial(1, 0.5, size=self.size) * 2 - 1.
 
-        # 識別器
-        if clsf is None:
-            self.clsf = LinearSVC(C=1.0)
-        else:
-            self.clsf = clsf
-
         # エネルギー関数
-        self.energy = self.H(self.s)
+        self.energy = CVError(self.s, self.X, self.y)
         self.acccnt = 0
-
-    # @jit(cache=True)
-    def H(self, sval=None):
-        '''Definition of 'Energy function', that is a CV err'''
-        skf = StratifiedKFold(n_splits=self.nsplits)
-        if sval is None:
-            sval = self.s
-        cidx = np.array(sval+1, np.bool)    # sval が ±1 で構成されていることを課程
-        scrs = []
-        for trn, tst in skf.split(self.X, self.y):
-            self.clsf.fit(self.X[trn][:, cidx], self.y[trn])
-            pred = self.clsf.predict(self.X[tst][:, cidx])
-            scrs.append(np.sum(self.y[tst] == pred) / (self.y[tst].shape[0]))
-        scrs = np.array(scrs)
-        return scrs.mean()
-
-    # @jit(cache=True)
-    def mcstep(self, value=None):
-        '''single MC step for trial value'''
-        if self.beta == 0.:  # 温度∞ (beta=0.0) は全とっかえ
-            self.s = binomial(1, 0.5, size=self.size) * 2 - 1.
-            self.energy = self.H(self.s)
-            self.acccnt += self.size
-            return
-        # 有限温度の場合
-        if value is None:
-            value = self.s
-        order = permutation(self.size)
-        rvals = uniform(0., 1., self.size)
-
-        for idx in order:
-            oldE = self.energy
-            self.s[idx] *= -1
-            newE = self.H(self.s)
-            delta = newE - oldE
-            pdelta = np.exp(-self.beta * delta)
-
-            if rvals[idx] < pdelta:
-                # 'accept' the state state
-                self.energy = newE
-                self.acccnt += 1
-            else:
-                # 'reject' restore
-                self.s[idx] *= -1
 
 
 class FeatureSelectionEMC:
@@ -142,8 +92,8 @@ class FeatureSelectionEMC:
     def __init__(self, dset, betas=None, nsplits=5, clsf=None):
         self.size = dset['X'].shape[1]
         if betas is None:
-            self.nbeta = 12
-            self.betas = [pow(1.25, l-11+1) for l in range(self.nbeta)]   # 決め打ち
+            self.nbeta = 16
+            self.betas = [pow(1.25, l-7+1) for l in range(self.nbeta)]   # 決め打ち
             self.betas[0] = 0.
             self.MCs = [FSsingleMC(dset, beta=beta, nsplits=nsplits, clsf=clsf)
                         for beta in self.betas]
@@ -155,8 +105,8 @@ class FeatureSelectionEMC:
     def mcexstep(self, isodd=False):
         '''A exchange MC step'''
         for mc in self.MCs:
-            #   mc.mcstep()
-            mc.s, mc.energy, mc.acccnt = MCstep(mc.s, mc.energy, mc.beta, mc.X, mc.y)
+            mc.energy, dummy = MCstep(mc.s, mc.energy, mc.beta, mc.X, mc.y)
+            mc.acccnt += dummy
 
         # exchange process
         if isodd:
@@ -208,10 +158,10 @@ if __name__ == '__main__':
     yy = np.array(wine['target'] == 0, dtype=np.float)
     model = FeatureSelectionEMC(dset={'X': XX, 'y': yy})
 
-    burn = model.trace(100)
-#   mclog = model.trace(5000)
+    burn = model.trace(5000)
+    mclog = model.trace(5000)
 
-#   np.savez('burnlog.npz', Betas=model.betas,
-#   Exlog=burn['Exlog'], Elog=burn['Elog'], Slog=burn['Slog'])
-#   np.savez('mclog.npz', Betas=model.betas,
-#   Exlog=mclog['Exlog'], Elog=mclog['Elog'], Slog=mclog['Slog'])
+    np.savez('burnlog.npz', Betas=model.betas,
+             Exlog=burn['Exlog'], Elog=burn['Elog'], Slog=burn['Slog'])
+    np.savez('mclog.npz', Betas=model.betas,
+             Exlog=mclog['Exlog'], Elog=mclog['Elog'], Slog=mclog['Slog'])
